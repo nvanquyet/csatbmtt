a8dcdfb1ab4d19e6fe51798fb0cc10a8e9fb4a81
VawnWuyest <nvanquyet810@gmail.com>
2025-03-06 15:29:18 +0700

    fix ip server

--- Shared/AppSettings/Config.cs
+++ Shared/AppSettings/Config.cs
@@ -2,8 +2,8 @@
 
 public static class Config
 {
-    public const string ServerIp = "127.0.0.1";
-    public const int ServerPort = 8080;
+    //public const string ServerIp = "127.0.0.1";
+    public const string ServerIp = "20.5.131.240";
     
     public const string DatabaseConnectionString = "mongodb://localhost:27017";
     public const string DatabaseString = "Cosoantoan";

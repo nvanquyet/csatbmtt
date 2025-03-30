da9b75b724cca4f15f6f35de4d3fd7250d2390cf
fl2k3xb <fl2k3xb@gmail.com>
2025-03-31 04:08:08 +0700

    update form home

--- Client/Form/HomeForm.Designer.cs
+++ Client/Form/HomeForm.Designer.cs
@@ -23,61 +23,64 @@ namespace Client.Form
 
         private void InitializeComponent()
         {
-            this.txtSearch = new TextBox();
-            this.lstChatHistory = new ListView();
-            this.btnLogout = new Button();
-            this.lstUserSuggestions = new ListBox();
-            this.SuspendLayout();
-            // 
-            // Form Settings
-            // 
-            this.Text = "Home";
-            this.ClientSize = new System.Drawing.Size(400, 300);
-            this.FormBorderStyle = FormBorderStyle.FixedDialog;
-            this.StartPosition = FormStartPosition.CenterScreen;
-            // 
-            // btnLogout
-            // 
-            this.btnLogout.Text = "Logout";
-            this.btnLogout.Size = new Size(75, 23);
-            this.btnLogout.Location = new Point(310, 10); // Góc trên bên phải
-            this.btnLogout.Click += new System.EventHandler(this.BtnLogout_Click);
-            this.Controls.Add(this.btnLogout);
+            txtSearch = new TextBox();
+            lstChatHistory = new ListView();
+            btnLogout = new Button();
+            lstUserSuggestions = new ListBox();
+            SuspendLayout();
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new Point(20, 40);
-            this.txtSearch.Width = 250;
-            this.txtSearch.TextChanged += new System.EventHandler(this.TxtSearch_TextChanged);
-            this.Controls.Add(this.txtSearch);
-            // 
-            // lstUserSuggestions
-            // 
-            this.lstUserSuggestions.Location = new Point(20, 65);
-            this.lstUserSuggestions.Size = new Size(340, 60);
-            this.lstUserSuggestions.Visible = false; // Ẩn ban đầu, sẽ hiển thị khi có dữ liệu gợi ý
-            lstUserSuggestions.DrawMode = DrawMode.OwnerDrawFixed;
-            lstUserSuggestions.DrawItem += lstUserSuggestions_DrawItem;
-            this.lstUserSuggestions.Click += new System.EventHandler(this.LstUserSuggestions_Click);
-            this.Controls.Add(this.lstUserSuggestions);
+            txtSearch.Location = new Point(20, 40);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(250, 23);
+            txtSearch.TabIndex = 1;
+            txtSearch.TextChanged += TxtSearch_TextChanged;
             // 
             // lstChatHistory
             // 
-            this.lstChatHistory.Location = new Point(20, 130);
-            this.lstChatHistory.Size = new Size(360, 150);
-            this.lstChatHistory.View = View.Details;
-            this.lstChatHistory.FullRowSelect = true;
-            this.lstChatHistory.Columns.Add("Username", 100);
-            this.lstChatHistory.Columns.Add("Last Handshake Time", 159);
-            this.lstChatHistory.Columns.Add("Status", 80); // Thêm cột Status
-            this.lstChatHistory.ItemActivate += new System.EventHandler(this.OnHandshakeUserActivated);
-            this.Controls.Add(this.lstChatHistory);
+            lstChatHistory.FullRowSelect = true;
+            lstChatHistory.Location = new Point(20, 92);
+            lstChatHistory.Name = "lstChatHistory";
+            lstChatHistory.Size = new Size(360, 188);
+            lstChatHistory.TabIndex = 3;
+            lstChatHistory.UseCompatibleStateImageBehavior = false;
+            lstChatHistory.View = View.Details;
+            lstChatHistory.ItemActivate += OnHandshakeUserActivated;
+            // 
+            // btnLogout
+            // 
+            btnLogout.Location = new Point(305, 39);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(75, 23);
+            btnLogout.TabIndex = 0;
+            btnLogout.Text = "Logout";
+            btnLogout.Click += BtnLogout_Click;
+            // 
+            // lstUserSuggestions
+            // 
+            lstUserSuggestions.DrawMode = DrawMode.OwnerDrawFixed;
+            lstUserSuggestions.Location = new Point(20, 65);
+            lstUserSuggestions.Name = "lstUserSuggestions";
+            lstUserSuggestions.Size = new Size(340, 52);
+            lstUserSuggestions.TabIndex = 2;
+            lstUserSuggestions.Visible = false;
+            lstUserSuggestions.Click += LstUserSuggestions_Click;
+            lstUserSuggestions.DrawItem += lstUserSuggestions_DrawItem;
             // 
             // HomeForm
             // 
-            this.ResumeLayout(false);
-            this.PerformLayout();
-            FormController.InitializeUiContext();
+            ClientSize = new Size(400, 300);
+            Controls.Add(btnLogout);
+            Controls.Add(txtSearch);
+            Controls.Add(lstUserSuggestions);
+            Controls.Add(lstChatHistory);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Name = "HomeForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Home";
+            ResumeLayout(false);
+            PerformLayout();
         }
     }
 }
